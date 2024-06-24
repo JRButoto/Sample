edc5dc933836820fca99547065f241f70276066a
JRButoto <johnbutoto@gmail.com>
2024-06-24 16:29:35 +0300

    added view get_child_nutrition_recomendations

--- backend/child/urls.py
+++ backend/child/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 
-from .views import getChildSummary, childStatistics
+from .views import getChildSummary, childStatistics, get_child_nutrition_recomendations
 
 app_name = 'child'
 
@@ -9,6 +9,7 @@ urlpatterns = [
 
     path('getChildSummary/', getChildSummary, name='getChildSummary'),
     path('childStatistics/', childStatistics, name='childStatistics'),
+    path('get_child_nutrition_recomendations/', get_child_nutrition_recomendations, name='get_child_nutrition_recomendations'),
  
 ]
 
--- backend/child/views.py
+++ backend/child/views.py
@@ -103,3 +103,134 @@ def childStatistics(request):
     }
 
     return Response(data)
+
+
+
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Child, Child_visit
+from cgmzscore.src.main import z_score_with_class
+import json
+from datetime import date
+
+# @csrf_exempt
+# def get_child_nutrition_recomendations(request):
+#     if request.method == 'POST':
+#         data = json.loads(request.body)
+#         registration_number = data.get('registration_number')
+
+#         try:
+#             child = Child.objects.get(child_number=registration_number)
+#             latest_visit = Child_visit.objects.filter(child=child).order_by('-date').first()
+            
+#             if not latest_visit:
+#                 return JsonResponse({'error': 'No visits found for this child'}, status=404)
+
+#             weight = latest_visit.weight_grams  # no need to convert grams to kg
+#             height = latest_visit.height
+#             birth_date = child.date_of_birth
+#             visit_date = latest_visit.date
+#             age_in_days = (visit_date - birth_date).days
+#             sex = 'M' if child.child_gender.lower() == 'male' else 'F'
+            
+#             score = z_score_with_class(weight=str(weight), muac="13.5", age_in_days=str(age_in_days), sex=sex, height=str(height))
+
+#             # Parse the JSON string into a dictionary
+#             score_data = json.loads(score)
+
+#             # Access the 'class_hfa' value
+#             class_hfa = score_data['class_hfa']
+
+#             # print(class_hfa)
+#             # print(age_in_days)
+            
+#             age_category = '0 to 6 months' if age_in_days <= 180 else \
+#                            '6 to 12 months' if age_in_days <= 365 else \
+#                            '1 to 3 years' if age_in_days <= 1095 else '3 to 5 years'
+            
+#             class_hfa = score_data['class_hfa'].strip()  # Remove leading/trailing spaces
+#             age_category = age_category.strip()  # Remove leading/trailing spaces   
+            
+#             classification = f"{class_hfa} && {age_category}"
+#             response_text = {
+#                 'Healthy && 0 to 6 months': "The child is healthy and is within the first 6 months of age.",
+#                 'Healthy && 6 to 12 months': "The child is healthy and is between 6 to 12 months old.",
+#                 'Healthy && 1 to 3 years': "The child is healthy and is between 1 to 3 years old.",
+#                 'Healthy && 3 to 5 years': "The child is healthy and is between 3 to 5 years old.",
+#                 'Moderately stunted && 0 to 6 months': "The child is moderately stunted and is within the first 6 months of age.",
+#                 'Moderately stunted && 6 to 12 months': "The child is moderately stunted and is between 6 to 12 months old.",
+#                 'Moderately stunted && 1 to 3 years': "The child is moderately stunted and is between 1 to 3 years old.",
+#                 'Moderately stunted && 3 to 5 years': "The child is moderately stunted and is between 3 to 5 years old.",
+#                 'Severely stunted && 0 to 6 months': "The child is severely stunted and is within the first 6 months of age.",
+#                 'Severely stunted && 6 to 12 months': "The child is severely stunted and is between 6 to 12 months old.",
+#                 'Severely stunted && 1 to 3 years': "The child is severely stunted and is between 1 to 3 years old.",
+#                 'Severely stunted && 3 to 5 years': "The child is severely stunted and is between 3 to 5 years old."
+#             }
+
+#             response_message = response_text.get(classification, "Classification not found.")
+#             return JsonResponse({'message': response_message})
+        
+#         except Child.DoesNotExist:
+#             return JsonResponse({'error': 'Child not found'}, status=404)
+        
+#     return JsonResponse({'error': 'Invalid request method'}, status=400)
+
+
+@csrf_exempt
+def get_child_nutrition_recomendations(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        registration_number = data.get('registration_number')
+
+        try:
+            child = Child.objects.get(child_number=registration_number)
+            latest_visit = Child_visit.objects.filter(child=child).order_by('-date').first()
+            
+            if not latest_visit:
+                return JsonResponse({'error': 'No visits found for this child'}, status=404)
+
+            weight = latest_visit.weight_grams  # no need to convert grams to kg
+            height = latest_visit.height
+            birth_date = child.date_of_birth
+            visit_date = latest_visit.date
+            age_in_days = (visit_date - birth_date).days
+            sex = 'M' if child.child_gender.lower() == 'male' else 'F'
+            
+            score = z_score_with_class(weight=str(weight), muac="13.5", age_in_days=str(age_in_days), sex=sex, height=str(height))
+
+            # Parse the JSON string into a dictionary
+            score_data = json.loads(score)
+
+            # Access the 'class_hfa' value
+            class_hfa = score_data['class_hfa'].strip().title()
+            
+            age_category = '0 to 6 months' if age_in_days <= 180 else \
+                           '6 to 12 months' if age_in_days <= 365 else \
+                           '1 to 3 years' if age_in_days <= 1095 else '3 to 5 years'
+            
+            classification = f"{class_hfa} && {age_category}"
+            
+            response_text = {
+                'Healthy && 0 to 6 months': "The child is healthy and is within the first 6 months of age.",
+                'Healthy && 6 to 12 months': "The child is healthy and is between 6 to 12 months old.",
+                'Healthy && 1 to 3 years': "The child is healthy and is between 1 to 3 years old.",
+                'Healthy && 3 to 5 years': "The child is healthy and is between 3 to 5 years old.",
+                'Moderately Stunted && 0 to 6 months': "The child is moderately stunted and is within the first 6 months of age.",
+                'Moderately Stunted && 6 to 12 months': "The child is moderately stunted and is between 6 to 12 months old.",
+                'Moderately Stunted && 1 to 3 years': "The child is moderately stunted and is between 1 to 3 years old.",
+                'Moderately Stunted && 3 to 5 years': "The child is moderately stunted and is between 3 to 5 years old.",
+                'Severely Stunted && 0 to 6 months': "The child is severely stunted and is within the first 6 months of age.",
+                'Severely Stunted && 6 to 12 months': "The child is severely stunted and is between 6 to 12 months old.",
+                'Severely Stunted && 1 to 3 years': "The child is severely stunted and is between 1 to 3 years old.",
+                'Severely Stunted && 3 to 5 years': "The child is severely stunted and is between 3 to 5 years old."
+            }
+
+            response_message = response_text.get(classification, "Classification not found.")
+            return JsonResponse({'message': response_message})
+        
+        except Child.DoesNotExist:
+            return JsonResponse({'error': 'Child not found'}, status=404)
+        
+    return JsonResponse({'error': 'Invalid request method'}, status=400)
+
+
